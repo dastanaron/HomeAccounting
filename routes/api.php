61dece653a2576643206ed8c8751e3e69f210593
dastanaron <flow199@yandex.ru>
2019-06-23 01:29:21 +0300

    request changed to any types

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/qr-code-scanner/send-check', 'QrCodeScannerController@sendCheck');
+Route::any('/qr-code-scanner/send-check', 'QrCodeScannerController@sendCheck');
