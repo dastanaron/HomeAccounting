be269ba0684266eaa401cc04dcbcf402802d680f
dastanaron <flow199@yandex.ru>
2022-06-02 13:18:40 +0300

    Updated containers #9

--- docker/runDocker.sh
+++ docker/runDocker.sh
@@ -9,10 +9,6 @@ docker run -d --name rabbit --network home_accounting --restart always -e RABBIT
 #BUILD cd /home/dastanaron/homeAccounting/repo/HomeAccounting/docker/php && docker build -t home_accounting -f Dockerfile.prod ../../
 docker run -d --name home_accounting --network home_accounting --restart always -v /home/dastanaron/homeAccounting/data/storage:/var/www/html/storage home_accounting:latest
 
-#BUILD_NGINX: cd /home/dastanaron/homeAccounting/repo/HomeAccounting/docker/nginx && docker build -t home_accounting_nginx -f Dockerfile.prod ../../
-docker run -d --name home_accounting_nginx --network home_accounting --restart always -p 8081:80 home_accounting_nginx:latest
-
 docker stop mysql && docker rm mysql
 docker stop rabbit && docker rm rabbit
-docker stop home_accounting_nginx && docker rm home_accounting_nginx
 docker stop home_accounting && docker rm home_accounting
